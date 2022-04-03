5c3a79e99d6ce0a4bc52f92744762c8e510d6a19
REAZN <reasonwhygamer@gmail.com>
2022-04-03 09:47:45 +0100

    roomcode styling

--- client/src/components/Roomcode.jsx
+++ client/src/components/Roomcode.jsx
@@ -1,11 +1,18 @@
 import React from "react";
+import { Clipboard } from "components"
 import style from "style/components/roomcode.module.scss";
 
 export default function Roomcode({ code }) {
 
     return (
-        <div className={style.roomcode}>
-            <span className={style.code}>{code}</span>
-        </div>
+        <>
+            {code ?
+                <div className={style.roomcode} onClick={() => navigator.clipboard.writeText(code)}>
+                    <span className={style.code}>{code}</span>
+                    <Clipboard className={style.icon} />
+                </div>
+                : ""
+            }
+        </>
     );
 }
